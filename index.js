575da5a3382a94fc13e23145d6e53efb4e8f0f0f
MatheusAndroid <56353600+MatheusAndroid@users.noreply.github.com>
2022-09-25 23:00:56 -0300

    backend finalizado, front-end em progresso

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ import veiculos from './models/veiculos.js';
 
 const sequelize = sql.db()
 const Veiculo = sequelize.define('veiculos', veiculos);
-
 await sequelize.sync()    
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+  next(); // Important
+})
 const port = 3000;
 
 
